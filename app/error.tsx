ccfea5de69bf36ba2d5e5e4f9b10e50533a93439
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-03-09 15:12:16 +0700

    feat: enhance error component with custom logo and improved button styling

--- app/error.tsx
+++ app/error.tsx
@@ -1,6 +1,9 @@
 "use client";
 
 import { useEffect } from "react";
+import { Button } from "@heroui/button";
+
+import Logo from "@/components/logo";
 
 export default function Error({
   error,
@@ -16,16 +19,17 @@ export default function Error({
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
+    <div className={`flex h-dvh flex-col items-center justify-center gap-4`}>
+      <Logo width={80} />
+      <h2 className={`text-2xl font-bold`}>Something went wrong!</h2>
+      <Button
+        onPress={
           // Attempt to recover by trying to re-render the segment
           () => reset()
         }
       >
         Try again
-      </button>
+      </Button>
     </div>
   );
 }
