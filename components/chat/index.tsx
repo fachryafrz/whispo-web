b23acf65ad6a9b3fd5a7698013c99982e91e529b
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-03-15 23:31:47 +0700

    Revert "feat: group chat messages by date for improved organization and readability (#30)"
    This reverts commit a47181bee128beff6ecef78d99c8b2f199cbbbca.

--- components/chat/index.tsx
+++ components/chat/index.tsx
@@ -17,30 +17,22 @@ export default function Chat() {
     useSelectedChat();
 
   useEffect(() => {
-    const isSmallScreen = window.innerWidth < 768;
-
     if (pathname === "/") {
       setShowChatRoom(false);
 
-      setTimeout(
-        () => {
-          clearSelectedChat();
-        },
-        isSmallScreen ? 500 : 0, // If the screen is small, wait 500ms before clearing the selected chat
-      );
+      setTimeout(() => {
+        clearSelectedChat();
+      }, 500);
     }
 
     const handleKeyDown = (e: KeyboardEvent) => {
       if (e.key === "Escape" && pathname !== "/") {
         setShowChatRoom(false);
 
-        setTimeout(
-          () => {
-            clearSelectedChat();
-            router.back();
-          },
-          isSmallScreen ? 500 : 0, // If the screen is small, wait 500ms before clearing the selected chat
-        );
+        setTimeout(() => {
+          clearSelectedChat();
+          router.back();
+        }, 500);
       }
     };
 
@@ -65,7 +57,7 @@ export default function Chat() {
     <>
       {selectedChat && (
         <section
-          className={`fixed inset-0 z-10 flex w-full flex-1 flex-col bg-white transition-all duration-500 dark:bg-black md:static md:z-0 ${showChatRoom ? "translate-x-0" : "translate-x-full md:translate-x-0"}`}
+          className={`fixed inset-0 z-10 flex w-full flex-1 flex-col bg-white transition-all duration-500 dark:bg-black md:static md:z-0 ${showChatRoom ? "translate-x-0 md:opacity-100" : "translate-x-full md:translate-x-0 md:opacity-0"}`}
         >
           {/* Header */}
           <ChatHeader />
--- components/chat/messages/index.tsx
+++ components/chat/messages/index.tsx
@@ -9,15 +9,12 @@ import {
 import { Spinner } from "@heroui/spinner";
 import { useInView } from "react-intersection-observer";
 import { useTheme } from "next-themes";
-import dayjs from "dayjs";
-import { Chip } from "@heroui/chip";
 
 import Message from "../message";
 
 import { Doc, Id } from "@/convex/_generated/dataModel";
 import { api } from "@/convex/_generated/api";
 import { useSelectedChat } from "@/zustand/selected-chat";
-import { groupMessagesByDate } from "@/utils/group-messages-by-date";
 
 const NUM_MESSAGES_TO_LOAD = 50;
 
@@ -41,8 +38,6 @@ export default function ChatMessages() {
   const containerRef = useRef<HTMLDivElement>(null);
   const [showScrollBtn, setShowScrollBtn] = useState<boolean>(false);
 
-  const groupedMessages = groupMessagesByDate(messages);
-
   const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
     setShowScrollBtn(e.currentTarget.scrollTop < 0);
   };
@@ -53,73 +48,65 @@ export default function ChatMessages() {
 
   return (
     <div className="relative flex-1 overflow-y-hidden before:absolute before:inset-0 before:bg-[url(/background/doodle.avif)] before:bg-[size:350px] before:bg-repeat before:opacity-15 before:dark:invert md:before:opacity-10">
-      {/* Grouped messages */}
-      {Object.entries(groupedMessages).map(([date, msgs]) => (
-        <div
-          key={date}
-          ref={containerRef}
-          className="relative flex h-full flex-1 flex-col-reverse items-center gap-1 overflow-y-auto p-4"
-          onScroll={handleScroll}
+      {/* Messages */}
+      <div
+        ref={containerRef}
+        className="relative flex h-full flex-1 flex-col-reverse items-center gap-1 overflow-y-auto p-4"
+        onScroll={handleScroll}
+      >
+        {/* Scroll to bottom */}
+        <Button
+          isIconOnly
+          className={`fixed z-10 bg-black text-white transition-all dark:bg-white dark:text-black ${showScrollBtn ? "pointer-events-auto opacity-100" : "pointer-events-none opacity-0"}`}
+          radius="full"
+          onPress={() => {
+            containerRef.current?.scrollTo({
+              top: 0,
+              behavior: "smooth",
+            });
+          }}
         >
-          {/* Scroll to bottom */}
-          <Button
-            isIconOnly
-            className={`fixed z-10 bg-black text-white transition-all dark:bg-white dark:text-black ${showScrollBtn ? "pointer-events-auto opacity-100" : "pointer-events-none opacity-0"}`}
-            radius="full"
-            onPress={() => {
-              containerRef.current?.scrollTo({
-                top: 0,
-                behavior: "smooth",
-              });
-            }}
-          >
-            <ArrowDown size={20} />
-          </Button>
+          <ArrowDown size={20} />
+        </Button>
 
-          {/* Messages */}
-          {msgs.map((msg, index) => {
-            const prevMsg = messages[index + 1];
-            const nextmsg = messages[index - 1];
-            const isDifferentSenderPrev = prevMsg?.senderId !== msg.senderId;
-            const isDifferentSenderNext = nextmsg?.senderId !== msg.senderId;
+        {messages.map((msg, index) => {
+          const prevMsg = messages[index + 1];
+          const nextmsg = messages[index - 1];
+          const isDifferentSenderPrev = prevMsg?.senderId !== msg.senderId;
+          const isDifferentSenderNext = nextmsg?.senderId !== msg.senderId;
 
-            return (
-              <div
-                key={msg._id}
-                className={`w-full ${prevMsg?.senderId} ${isDifferentSenderPrev ? "pt-4" : "pt-0"}`}
-              >
-                {/* Message bubble */}
-                <Message
-                  currentUser={currentUser as Doc<"users">}
-                  index={index}
-                  isDifferentSenderNext={isDifferentSenderNext}
-                  isDifferentSenderPrev={isDifferentSenderPrev}
-                  msg={msg}
-                />
+          return (
+            <div
+              key={msg._id}
+              className={`w-full ${prevMsg?.senderId} ${isDifferentSenderPrev ? "pt-4" : "pt-0"}`}
+            >
+              <Message
+                currentUser={currentUser as Doc<"users">}
+                index={index}
+                isDifferentSenderNext={isDifferentSenderNext}
+                isDifferentSenderPrev={isDifferentSenderPrev}
+                msg={msg}
+              />
 
-                {/* Edited */}
-                {msg.isEdited && (
-                  <span
-                    className={`flex text-xs ${msg.senderId === currentUser?._id ? "justify-end" : "justify-start"}`}
-                  >
-                    Edited
-                  </span>
-                )}
-              </div>
-            );
-          })}
-
-          {/* Message date */}
-          <Chip size="sm">{dayjs(date).format("dddd, DD MMMM YYYY")}</Chip>
-
-          {/* Paginate messages */}
-          {status === "CanLoadMore" && (
-            <div ref={loadMoreRef}>
-              <Spinner color={resolvedTheme === "dark" ? "white" : "primary"} />
+              {/* Edited */}
+              {msg.isEdited && (
+                <span
+                  className={`flex text-xs ${msg.senderId === currentUser?._id ? "justify-end" : "justify-start"}`}
+                >
+                  Edited
+                </span>
+              )}
             </div>
-          )}
-        </div>
-      ))}
+          );
+        })}
+
+        {/* Paginate messages */}
+        {status === "CanLoadMore" && (
+          <div ref={loadMoreRef}>
+            <Spinner color={resolvedTheme === "dark" ? "white" : "primary"} />
+          </div>
+        )}
+      </div>
     </div>
   );
 }
