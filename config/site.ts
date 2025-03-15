5d4ea2a92cccebbd78f34e68262870ec9c7ffd74
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-03-15 18:26:26 +0700

    chore: update application description for clarity and conciseness

--- config/site.ts
+++ config/site.ts
@@ -3,6 +3,6 @@ export type SiteConfig = typeof siteConfig;
 export const siteConfig = {
   name: "Whispo",
   description:
-    'Whispo is a fast, secure, and intuitive real-time chat application. Inspired by the word "whisper", Whispo represents quiet, seamless, and private conversations. The logo, resembling a cloud, symbolizes the fluidity of messages, just like whispers in the air, and reflects the cloud-based real-time nature of the application. With seamless authentication, real-time messaging, and an elegant UI, Whispo ensures a smooth chat experience for users.',
+    'Whispo is a fast, secure, and intuitive real-time chat application designed to enhance communication. With seamless authentication, real-time messaging, and an elegant UI, Whispo ensures a smooth chat experience for users.',
   url: process.env.NEXT_PUBLIC_APP_URL,
 };
