d5342c001c96a2491484b16975c8f484beb62331
Fachry Dwi Afriza <fachry.dwi.fd.fda@gmail.com>
2025-03-16 00:06:04 +0700

    fix: rendering the messages (#31)
    * feat: group chat messages by date for improved organization and readability
    
    * fix: rendering the messages

--- components/chat/messages/index.tsx
+++ components/chat/messages/index.tsx
@@ -9,12 +9,15 @@ import {
 import { Spinner } from "@heroui/spinner";
 import { useInView } from "react-intersection-observer";
 import { useTheme } from "next-themes";
+import dayjs from "dayjs";
+import { Chip } from "@heroui/chip";
 
 import Message from "../message";
 
 import { Doc, Id } from "@/convex/_generated/dataModel";
 import { api } from "@/convex/_generated/api";
 import { useSelectedChat } from "@/zustand/selected-chat";
+import { groupMessagesByDate } from "@/utils/group-messages-by-date";
 
 const NUM_MESSAGES_TO_LOAD = 50;
 
@@ -38,6 +41,8 @@ export default function ChatMessages() {
   const containerRef = useRef<HTMLDivElement>(null);
   const [showScrollBtn, setShowScrollBtn] = useState<boolean>(false);
 
+  const groupedMessages = groupMessagesByDate(messages);
+
   const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
     setShowScrollBtn(e.currentTarget.scrollTop < 0);
   };
@@ -48,64 +53,78 @@ export default function ChatMessages() {
 
   return (
     <div className="relative flex-1 overflow-y-hidden before:absolute before:inset-0 before:bg-[url(/background/doodle.avif)] before:bg-[size:350px] before:bg-repeat before:opacity-15 before:dark:invert md:before:opacity-10">
-      {/* Messages */}
       <div
         ref={containerRef}
-        className="relative flex h-full flex-1 flex-col-reverse items-center gap-1 overflow-y-auto p-4"
+        className="relative flex h-full flex-1 flex-col-reverse overflow-y-auto p-4"
         onScroll={handleScroll}
       >
-        {/* Scroll to bottom */}
-        <Button
-          isIconOnly
-          className={`fixed z-10 bg-black text-white transition-all dark:bg-white dark:text-black ${showScrollBtn ? "pointer-events-auto opacity-100" : "pointer-events-none opacity-0"}`}
-          radius="full"
-          onPress={() => {
-            containerRef.current?.scrollTo({
-              top: 0,
-              behavior: "smooth",
-            });
-          }}
-        >
-          <ArrowDown size={20} />
-        </Button>
-
-        {messages.map((msg, index) => {
-          const prevMsg = messages[index + 1];
-          const nextmsg = messages[index - 1];
-          const isDifferentSenderPrev = prevMsg?.senderId !== msg.senderId;
-          const isDifferentSenderNext = nextmsg?.senderId !== msg.senderId;
-
-          return (
-            <div
-              key={msg._id}
-              className={`w-full ${prevMsg?.senderId} ${isDifferentSenderPrev ? "pt-4" : "pt-0"}`}
+        {/* Grouped messages */}
+        {Object.entries(groupedMessages).map(([date, msgs]) => (
+          <div
+            key={date}
+            className="flex w-full flex-col-reverse items-center gap-1"
+          >
+            {/* Scroll to bottom */}
+            <Button
+              isIconOnly
+              className={`fixed z-10 bg-black text-white transition-all dark:bg-white dark:text-black ${showScrollBtn ? "pointer-events-auto opacity-100" : "pointer-events-none opacity-0"}`}
+              radius="full"
+              onPress={() => {
+                containerRef.current?.scrollTo({
+                  top: 0,
+                  behavior: "smooth",
+                });
+              }}
             >
-              <Message
-                currentUser={currentUser as Doc<"users">}
-                index={index}
-                isDifferentSenderNext={isDifferentSenderNext}
-                isDifferentSenderPrev={isDifferentSenderPrev}
-                msg={msg}
-              />
+              <ArrowDown size={20} />
+            </Button>
 
-              {/* Edited */}
-              {msg.isEdited && (
-                <span
-                  className={`flex text-xs ${msg.senderId === currentUser?._id ? "justify-end" : "justify-start"}`}
+            {/* Messages */}
+            {msgs.map((msg, index) => {
+              const prevMsg = msgs[index + 1];
+              const nextMsg = msgs[index - 1];
+              const isDifferentSenderPrev = prevMsg?.senderId !== msg.senderId;
+              const isDifferentSenderNext = nextMsg?.senderId !== msg.senderId;
+
+              return (
+                <div
+                  key={msg._id}
+                  className={`w-full ${prevMsg?.senderId} ${isDifferentSenderPrev ? "pt-4" : "pt-0"}`}
                 >
-                  Edited
-                </span>
-              )}
-            </div>
-          );
-        })}
+                  {/* Message bubble */}
+                  <Message
+                    currentUser={currentUser as Doc<"users">}
+                    index={index}
+                    isDifferentSenderNext={isDifferentSenderNext}
+                    isDifferentSenderPrev={isDifferentSenderPrev}
+                    msg={msg}
+                  />
 
-        {/* Paginate messages */}
-        {status === "CanLoadMore" && (
-          <div ref={loadMoreRef}>
-            <Spinner color={resolvedTheme === "dark" ? "white" : "primary"} />
+                  {/* Edited */}
+                  {msg.isEdited && (
+                    <span
+                      className={`flex text-xs ${msg.senderId === currentUser?._id ? "justify-end" : "justify-start"}`}
+                    >
+                      Edited
+                    </span>
+                  )}
+                </div>
+              );
+            })}
+
+            {/* Message date */}
+            <Chip size="sm">{dayjs(date).format("dddd, DD MMMM YYYY")}</Chip>
+
+            {/* Paginate messages */}
+            {status === "CanLoadMore" && (
+              <div ref={loadMoreRef}>
+                <Spinner
+                  color={resolvedTheme === "dark" ? "white" : "primary"}
+                />
+              </div>
+            )}
           </div>
-        )}
+        ))}
       </div>
     </div>
   );
